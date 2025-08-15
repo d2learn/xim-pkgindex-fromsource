5a71f398861e796033def63225d69b454ee4c9d4
sunrisepeak <speakshen@163.com>
2025-08-15 22:59:14 +0800

    musl-gcc update

--- pkgs/m/musl-gcc.lua
+++ pkgs/m/musl-gcc.lua
@@ -20,7 +20,7 @@ package = {
     categories = {"compiler", "gnu", "toolchain", "libc", "musl" },
     keywords = {"compiler", "gnu", "gcc", "toolchain", "c", "c++", "musl", "libc"},
     programs = {
-        "musl-gcc-static", "musl-g++-static",
+        --"musl-gcc-static", "musl-g++-static",
         "musl-gcc", "musl-g++", "musl-c++", "musl-cpp",
         "musl-addr2line", "musl-ar", "musl-as", "musl-ld", "musl-nm",
         "musl-objcopy", "musl-objdump", "musl-ranlib", "musl-readelf",
@@ -69,6 +69,69 @@ function config()
             alias = program_fullname,
         })
     end
+
+-- runtime lib
+    log.warn("add runtime libraries for musl-gcc-static...")
+    local musl_lib_dir = path.join(
+        pkginfo.install_dir(),
+        "x86_64-linux-musl", "lib"
+    )
+
+    -- add musl's libc libc.so and libstdc++.so.6 , libgcc_s.so.1
+    xvm.add("musl-libc", {
+        version = "musl-gcc-" .. pkginfo.version(),
+        filename = "libc.so",
+        bindir = musl_lib_dir,
+        type = "lib",
+        alias = "libc.so",
+    })
+
+    xvm.add("libstdc++", {
+        version = "musl-gcc-" .. pkginfo.version(),
+        filename = "libstdc++.so.6",
+        bindir = musl_lib_dir,
+        type = "lib",
+        alias = "libstdc++.so.6",
+    })
+
+    xvm.add("libgcc_s", {
+        version = "musl-gcc-" .. pkginfo.version(),
+        filename = "libgcc_s.so.1",
+        bindir = musl_lib_dir,
+        type = "lib",
+        alias = "libgcc_s.so.1",
+    })
+
+    -- add ld.so (musl's ld.so wrapper)
+    xvm.add("ld-musl", {
+        version = "musl-gcc-" .. pkginfo.version(),
+        filename = "ld-musl-x86_64.so.1",
+        bindir = musl_lib_dir,
+        type = "lib",
+        alias = "libc.so",
+    })
+
+-- special commands
+    xvm.add("musl-ldd", {
+        version = "musl-gcc-" .. pkginfo.version(),
+        bindir = musl_lib_dir,
+        alias = "libc.so --list",
+        envs = {
+            -- ? alias = "libc.so --library-path musl_lib_dir --list",
+            LD_LIBRARY_PATH = musl_lib_dir,
+        }
+    })
+
+    xvm.add("musl-loader", {
+        version = "musl-gcc-" .. pkginfo.version(),
+        bindir = musl_lib_dir,
+        alias = "libc.so",
+        envs = {
+            -- ? alias = "libc.so --library-path musl_lib_dir",
+            LD_LIBRARY_PATH = musl_lib_dir,
+        }
+    })
+
     xvm.add("musl-gcc-static", {
         bindir = program_bindir,
         alias = "x86_64-linux-musl-gcc -static",
@@ -86,6 +149,14 @@ function uninstall()
         xvm.remove(program)
         xvm.remove("x86_64-linux-" .. program)
     end
+    -- runtime libraries
+    xvm.remove("musl-libc", "musl-gcc-" .. pkginfo.version())
+    xvm.remove("ld-musl", "musl-gcc-" .. pkginfo.version())
+    xvm.remove("libstdc++", "musl-gcc-" .. pkginfo.version())
+    xvm.remove("libgcc_s", "musl-gcc-" .. pkginfo.version())
+    -- ld.so wrapper
+    xvm.remove("musl-ldd", "musl-gcc-" .. pkginfo.version())
+    xvm.remove("musl-loader", "musl-gcc-" .. pkginfo.version())
     xvm.remove("musl-gcc-static")
     xvm.remove("musl-g++-static")
     return true
