a1b90c8fb401fa7d289b2c171f07b2e57f55f906
sunrisepeak <speakshen@163.com>
2026-05-01 02:34:19 +0800

    fix(util-linux): pin deps to namespaces

--- pkgs/u/util-linux.lua
+++ pkgs/u/util-linux.lua
@@ -32,10 +32,12 @@ package = {
     xpm = {
         linux = {
             deps = {
-                "xpkg-helper@0.0.1", "gcc@15.1.0", "make@4.3",
-                "configure-project-installer@0.0.1",
-                "linux-headers@5.11.1",
-                "ncurses@6.4", -- for libtinfo
+                "xim:xpkg-helper@0.0.1",
+                "xim:configure-project-installer@0.0.1",
+                "xim:gcc@15.1.0",
+                "xim:make@4.3",
+                "xim:linux-headers@5.11.1",
+                "fromsource:ncurses@6.4",       -- for libtinfo
             },
             ["latest"] = { ref = "2.39.3" },
             ["2.39.3"] = {},
