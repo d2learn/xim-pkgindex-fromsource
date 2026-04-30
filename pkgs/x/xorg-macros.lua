a6ec52934b61c69043c3ed9a79a427d5d7f58aea
sunrisepeak <speakshen@163.com>
2026-05-01 02:10:17 +0800

    fix(xorg-macros): pin deps to xim namespace + use $(nproc)
    Standard Wave 1 cleanup: namespace all deps, replace make -j24 with
    os.cpuinfo("ncpu").

--- pkgs/x/xorg-macros.lua
+++ pkgs/x/xorg-macros.lua
@@ -33,7 +33,11 @@ package = {
 
     xpm = {
         linux = {
-            deps = { "xpkg-helper@0.0.1", "gcc@15.1.0", "make@4.3" },
+            deps = {
+                "xim:xpkg-helper@0.0.1",
+                "xim:gcc@15.1.0",
+                "xim:make@4.3",
+            },
             ["latest"] = { ref = "1.20.1" },
             ["1.20.1"] = {
                 url = {
@@ -67,7 +71,7 @@ function install()
         " --prefix=" .. prefix)
 
     log.info("3.Building xorg-macros...")
-    system.exec("make -j24")
+    system.exec(string.format("make -j%d", os.cpuinfo("ncpu") or 4))
 
     log.info("4.Installing xorg-macros...")
     system.exec("make install")
