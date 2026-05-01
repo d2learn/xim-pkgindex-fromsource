d1bfb4c3aaef6e813b55ed15198c11f7811a721e
SPeak <speakshen@163.com>
2026-05-01 18:51:34 +0800

    fix(libxau): sandbox template + ACLOCAL/PKG_CONFIG export (#58)
    Apply bzip2 #49 sandbox-fix template; use shell glob for libXau*.so*
    enumeration and xau*.pc copy.
    
    Verified in iso (xlings 0.4.9):
    - install OK; install_dir/lib has libXau.so/libXau.so.6/libXau.so.6.0.0
    - <subos>/usr/include/X11/Xauth.h present
    - <subos>/usr/lib/pkgconfig/xau.pc present

--- pkgs/l/libxau.lua
+++ pkgs/l/libxau.lua
@@ -58,53 +58,54 @@ import("xim.libxpkg.pkginfo")
 import("xim.libxpkg.log")
 import("xim.libxpkg.xvm")
 
+-- Sandbox file enumeration helper (os.files(glob) is sandbox-nil).
+local function _ls_glob(globpat)
+    local out = {}
+    local h = io.popen("ls -1 " .. globpat .. " 2>/dev/null")
+    if not h then return out end
+    for line in h:lines() do
+        if line ~= "" then table.insert(out, path.filename(line)) end
+    end
+    h:close()
+    return out
+end
+
+local function _sys_usr_includedir()
+    return path.join(system.subos_sysrootdir(), "usr/include")
+end
+local function _sys_usr_libdir()
+    return path.join(system.subos_sysrootdir(), "usr/lib")
+end
+
 local function libxau_libs()
     local libdir = path.join(pkginfo.install_dir(), "lib/x86_64-linux-gnu")
     if not os.isdir(libdir) then
         libdir = path.join(pkginfo.install_dir(), "lib")
     end
-    
-    local out = {}
-    
-    -- Scan for all libXau*.so* files
-    for _, file in ipairs(os.files(path.join(libdir, "libXau*.so*"))) do
-        local name = path.filename(file)
-        table.insert(out, name)
-    end
-    
-    -- If no files found via glob, return default set
+    local out = _ls_glob(path.join(libdir, "libXau*.so*"))
     if #out == 0 then
-        table.insert(out, "libXau.so")
-        table.insert(out, "libXau.so.6")
+        out = { "libXau.so", "libXau.so.6" }
     end
-    
     return out
 end
 
-local sys_usr_includedir = path.join(system.subos_sysrootdir(), "usr/include")
-
 function install()
-    local scode_libxau_dir = path.absolute("libXau-" .. pkginfo.version())
-    local build_libxau_dir = "build-libxau"
+    -- sandbox template (#49 bzip2)
+    local runtime_dir = path.directory(pkginfo.install_file())
+    local scode_dir = path.join(runtime_dir, "libXau-" .. pkginfo.version())
+    local build_dir = path.join(runtime_dir, "build-libxau")
+    local prefix = pkginfo.install_dir()
 
-    log.info("1.Creating build dir -" .. build_libxau_dir)
-    os.tryrm(build_libxau_dir)
-    os.mkdir(build_libxau_dir)
+    os.tryrm(build_dir)
+    os.mkdir(build_dir)
 
-    log.info("2.Configuring libxau with autotools...")
-    os.cd(build_libxau_dir)
-    local libxau_prefix = pkginfo.install_dir()
-    system.exec("" .. scode_libxau_dir .. "/configure"
-        .. " --prefix=" .. libxau_prefix
-        .. " --disable-static"
-        .. " --enable-shared"
-    )
-
-    log.info("3.Building libxau...")
-    system.exec(string.format("make -j%d", os.cpuinfo("ncpu") or 4))
-
-    log.info("4.Installing libxau...")
-    system.exec("make install")
+    log.info("Configuring + building + installing libxau (autotools)...")
+    system.exec(string.format(
+        "sh -c 'export PKG_CONFIG_PATH=%s/usr/lib/pkgconfig; "
+        .. "cd %s && %s/configure --prefix=%s --disable-static --enable-shared "
+        .. "&& make -j8 && make install'",
+        system.subos_sysrootdir(), build_dir, scode_dir, prefix
+    ))
 
     return os.isdir(pkginfo.install_dir())
 end
@@ -116,63 +117,50 @@ function config()
         libdir = path.join(pkginfo.install_dir(), "lib")
     end
 
-    local config = {
+    local cfg = {
         type = "lib",
         version = "libxau-" .. pkginfo.version(),
         bindir = libdir,
     }
 
     for _, lib in ipairs(libxau_libs()) do
-        config.alias = lib
-        config.filename = lib
-        xvm.add(lib, config)
+        cfg.alias = lib
+        cfg.filename = lib
+        xvm.add(lib, cfg)
     end
 
     log.info("Adding header files to sysroot...")
-    local libxau_hdr_dir = path.join(pkginfo.install_dir(), "include")
-    os.mkdir(sys_usr_includedir)
-
-    -- Copy X11 headers
-    local x11_include_dir = path.join(libxau_hdr_dir, "X11")
-    if os.isdir(x11_include_dir) then
-        os.cp(x11_include_dir, sys_usr_includedir, { force = true })
+    local sys_inc = _sys_usr_includedir()
+    os.mkdir(sys_inc)
+    local x11_dir = path.join(pkginfo.install_dir(), "include", "X11")
+    if os.isdir(x11_dir) then
+        os.cp(x11_dir, sys_inc, { force = true })
     end
 
-    -- Copy pkgconfig files
-    local sys_pc_dir = path.join(system.subos_sysrootdir(), "usr/lib/pkgconfig")
+    local sys_pc_dir = path.join(_sys_usr_libdir(), "pkgconfig")
     os.mkdir(sys_pc_dir)
-    local pc_dirs = {
-        path.join(pkginfo.install_dir(), "lib/pkgconfig"),
-        path.join(pkginfo.install_dir(), "lib/x86_64-linux-gnu/pkgconfig"),
-    }
-    for _, pc_dir in ipairs(pc_dirs) do
-        if os.isdir(pc_dir) then
-            for _, pc in ipairs(os.files(path.join(pc_dir, "xau*.pc"))) do
-                os.cp(pc, sys_pc_dir)
-            end
+    for _, pc_subdir in ipairs({"lib/pkgconfig", "lib/x86_64-linux-gnu/pkgconfig"}) do
+        local src = path.join(pkginfo.install_dir(), pc_subdir)
+        if os.isdir(src) then
+            system.exec(string.format(
+                "sh -c 'cp -f %s/xau*.pc %s/ 2>/dev/null || true'",
+                src, sys_pc_dir
+            ))
         end
     end
 
     xvm.add("libxau")
-
     return true
 end
 
 function uninstall()
     xvm.remove("libxau")
-
     for _, lib in ipairs(libxau_libs()) do
         xvm.remove(lib, "libxau-" .. pkginfo.version())
     end
-
-    -- Remove header files (only Xauth.h)
-    os.tryrm(path.join(sys_usr_includedir, "X11/Xauth.h"))
-
-    -- Remove pkgconfig files
-    local sys_pc_dir = path.join(system.subos_sysrootdir(), "usr/lib/pkgconfig")
-    for _, pc in ipairs(os.files(path.join(sys_pc_dir, "xau*.pc"))) do
-        os.tryrm(pc)
-    end
-
+    os.tryrm(path.join(_sys_usr_includedir(), "X11/Xauth.h"))
+    system.exec(string.format(
+        "sh -c 'rm -f %s/pkgconfig/xau*.pc'", _sys_usr_libdir()
+    ))
     return true
 end
