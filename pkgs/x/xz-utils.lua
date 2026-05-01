a6f205e1272c4c88933dce478104c5ac9f360774
sunrisepeak <speakshen@163.com>
2026-05-01 22:17:43 +0800

    feat(xz-utils): drop configure-project-installer, inline autotools build
    Switch fromsource:xz-utils from the legacy xim:configure-project-installer
    shim flow to its own inline autotools build, applying the cluster-A
    sandbox-fix template:
    
    * paths derived from pkginfo.install_file() (path.absolute is nil)
    * configure + make + make install chained in a single `sh -c` (os.cd
      doesn't propagate to system.exec children)
    * fixed -j8 (os.cpuinfo is nil)
    * sysroot include dir wrapped in _sys_usr_includedir() helper (top-level
      evaluation may run before xlings activates the sandbox subos)
    * lzma.h copy uses shell cp instead of os.cd + os.cp(file, dest)
    * declare upstream url + license corrected to 0BSD
    
    Verified end-to-end as a fromsource:gcc@16.1.0 dep in an isolated
    xlings 0.4.9 environment: configure -> make -j8 -> make install -> liblzma
    headers/libs in place, downstream gcc build succeeds.
    
    This is the first cluster-B leaf; remaining cluster-B packages
    (zlib, ncurses, openssl, libffi, readline, util-linux, automake, python)
    will follow.

--- pkgs/x/xz-utils.lua
+++ pkgs/x/xz-utils.lua
@@ -1,3 +1,7 @@
+function __xz_url(version)
+    return format("https://tukaani.org/xz/xz-%s.tar.gz", version)
+end
+
 package = {
     spec = "1",
 
@@ -7,7 +11,7 @@ package = {
     description = "XZ Utils: lossless compression software with liblzma library",
 
     authors = "Lasse Collin",
-    licenses = "https://tukaani.org/xz/xz-file-format.txt",
+    licenses = "0BSD",
     repo = "https://git.tukaani.org/xz.git/",
 
     type = "package",
@@ -24,12 +28,14 @@ package = {
         linux = {
             deps = {
                 "xim:xpkg-helper@0.0.1",
-                "xim:configure-project-installer@0.0.1",
                 "xim:gcc@15.1.0",
                 "xim:make@4.3",
             },
             ["latest"] = { ref = "5.4.5" },
-            ["5.4.5"] = {},
+            ["5.4.5"] = {
+                url = __xz_url("5.4.5"),
+                sha256 = nil,
+            },
         },
     },
 }
@@ -46,13 +52,29 @@ local libs = {
     "liblzma.a",
 }
 
-local sys_usr_includedir = path.join(system.subos_sysrootdir(), "usr/include")
+local function _sys_usr_includedir()
+    return path.join(system.subos_sysrootdir(), "usr/include")
+end
 
 function install()
-    local xpkg = package.name .. "@" .. pkginfo.version()
-    os.tryrm(pkginfo.install_dir())
-    system.exec("configure-project-installer " .. pkginfo.install_dir()
-    .. " --xpkg-scode " .. xpkg)
+    -- Sandbox template (PR #49 bzip2): derive paths from pkginfo.install_file()
+    -- since path.absolute is nil; chain configure+make+install in single sh -c
+    -- because os.cd doesn't propagate to system.exec children.
+    local runtime_dir = path.directory(pkginfo.install_file())
+    local scode_dir = path.join(runtime_dir, "xz-" .. pkginfo.version())
+    local build_dir = path.join(runtime_dir, "build-xz-utils")
+    local prefix = pkginfo.install_dir()
+
+    os.tryrm(build_dir)
+    os.mkdir(build_dir)
+
+    log.info("Configuring + building + installing xz-utils (autotools)...")
+    system.exec(string.format(
+        "sh -c 'cd %s && %s/configure --prefix=%s --disable-static --enable-shared "
+        .. "&& make -j8 && make install'",
+        build_dir, scode_dir, prefix
+    ))
+
     return os.isdir(pkginfo.install_dir())
 end
 
@@ -84,15 +106,19 @@ function config()
     end
 
     log.warn("add header files to sysroot...")
+    local sys_inc = _sys_usr_includedir()
     local includedir = path.join(pkginfo.install_dir(), "include", "lzma")
-    
-    os.tryrm(path.join(sys_usr_includedir, "lzma"))
-    os.cp(includedir, path.join(sys_usr_includedir, "lzma"), { force = true})
 
-    os.cd(path.join(pkginfo.install_dir(), "include"))
-    os.cp("lzma.h", sys_usr_includedir)
+    os.tryrm(path.join(sys_inc, "lzma"))
+    os.cp(includedir, path.join(sys_inc, "lzma"), { force = true })
 
-    xvm.add("xz-utils", { binding = version_tag})
+    -- copy lzma.h via shell (os.cd + os.cp(file, dest) pattern can mis-resolve in sandbox)
+    system.exec(string.format(
+        "sh -c 'cp -f %s/include/lzma.h %s/lzma.h'",
+        pkginfo.install_dir(), sys_inc
+    ))
+
+    xvm.add("xz-utils", { binding = version_tag })
 
     return true
 end
@@ -107,8 +133,9 @@ function uninstall()
     for _, lib in ipairs(libs) do
         xvm.remove(lib, "xz-" .. pkginfo.version())
     end
-    os.tryrm(path.join(sys_usr_includedir, "lzma.h"))
-    os.tryrm(path.join(sys_usr_includedir, "lzma"))
+    local sys_inc = _sys_usr_includedir()
+    os.tryrm(path.join(sys_inc, "lzma.h"))
+    os.tryrm(path.join(sys_inc, "lzma"))
 
     xvm.remove("xz-utils-binding-tree")
 
