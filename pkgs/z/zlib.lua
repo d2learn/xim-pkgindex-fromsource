3445ea0f82627d594bf25e64e2799dd9dcce95da
SPeak <speakshen@163.com>
2026-05-02 01:41:04 +0800

    feat(zlib): drop configure-project-installer, inline configure+make build (#62)
    Switch fromsource:zlib from the legacy xim:configure-project-installer
    shim flow to its own inline build, applying the cluster-A sandbox-fix
    template:
    
    * paths derived from pkginfo.install_file() (path.absolute is nil)
    * configure + make + make install chained in a single sh -c (zlib's
      configure does not support out-of-tree build, run from srcdir)
    * fixed -j8 (os.cpuinfo is nil)
    * sysroot include dir wrapped in _sys_usr_includedir() helper
    * zlib.h / zconf.h copy uses shell cp instead of os.cd + os.cp(file)
    * declare upstream url + sha256 (mirroring scode:zlib's github source)
    * license corrected: was a URL, now SPDX "Zlib"
    
    Verified end-to-end in an isolated xlings 0.4.9 environment:
      ✓ configure -> make -j8 -> make install
      ✓ produced libz.so.1.3.1 + libz.a + libz.so/libz.so.1 symlinks
      ✓ headers staged into install_dir/include
      ✓ "8 package(s) installed" exit 0
    
    Cluster B leaf 1/8.

--- pkgs/z/zlib.lua
+++ pkgs/z/zlib.lua
@@ -1,3 +1,7 @@
+function __zlib_url(version)
+    return format("https://github.com/madler/zlib/releases/download/v%s/zlib-%s.tar.gz", version, version)
+end
+
 package = {
     spec = "1",
 
@@ -8,7 +12,7 @@ package = {
     description = "A Massively Spiffy Yet Delicately Unobtrusive Compression Library",
 
     authors = "Jean-loup Gailly, Mark Adler",
-    licenses = "https://zlib.net/zlib_license.html",
+    licenses = "Zlib",
     repo = "https://github.com/madler/zlib",
 
     -- xim pkg info
@@ -26,12 +30,14 @@ package = {
         linux = {
             deps = {
                 "xim:xpkg-helper@0.0.1",
-                "xim:configure-project-installer@0.0.1",
                 "xim:gcc@15.1.0",
                 "xim:make@4.3",
             },
             ["latest"] = { ref = "1.3.1" },
-            ["1.3.1"] = { },
+            ["1.3.1"] = {
+                url = __zlib_url("1.3.1"),
+                sha256 = "9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df23",
+            },
         },
     },
 }
@@ -48,13 +54,24 @@ local libs = {
     "libz.a",
 }
 
-local sys_usr_includedir = path.join(system.subos_sysrootdir(), "usr/include")
+local function _sys_usr_includedir()
+    return path.join(system.subos_sysrootdir(), "usr/include")
+end
 
 function install()
-    local xpkg = package.name .. "@" .. pkginfo.version()
-    os.tryrm(pkginfo.install_dir())
-    system.exec("configure-project-installer " .. pkginfo.install_dir()
-    .. " --xpkg-scode " .. xpkg)
+    -- Sandbox template (PR #49 bzip2): derive paths from pkginfo.install_file()
+    -- since path.absolute is nil; chain configure + make + install in single
+    -- sh -c (zlib's configure does not support out-of-tree build, run from srcdir).
+    local runtime_dir = path.directory(pkginfo.install_file())
+    local scode_dir = path.join(runtime_dir, "zlib-" .. pkginfo.version())
+    local prefix = pkginfo.install_dir()
+
+    log.info("Configuring + building + installing zlib...")
+    system.exec(string.format(
+        "sh -c 'cd %s && ./configure --prefix=%s && make -j8 && make install'",
+        scode_dir, prefix
+    ))
+
     return os.isdir(pkginfo.install_dir())
 end
 
@@ -80,9 +97,12 @@ function config()
 
     log.warn("add zlib.h to sysroot/usr/include ...")
 
-    os.cd(path.join(pkginfo.install_dir(), "include"))
-    os.cp("zlib.h", sys_usr_includedir)
-    os.cp("zconf.h", sys_usr_includedir)
+    -- shell cp: avoid os.cd + os.cp(file, dest) sandbox edge cases
+    local sys_inc = _sys_usr_includedir()
+    system.exec(string.format(
+        "sh -c 'cp -f %s/include/zlib.h %s/include/zconf.h %s/'",
+        pkginfo.install_dir(), pkginfo.install_dir(), sys_inc
+    ))
 
     return true
 end
@@ -94,8 +114,9 @@ function uninstall()
         xvm.remove(lib, "zlib-" .. pkginfo.version())
     end
 
-    os.tryrm(path.join(sys_usr_includedir, "zlib.h"))
-    os.tryrm(path.join(sys_usr_includedir, "zconf.h"))
+    local sys_inc = _sys_usr_includedir()
+    os.tryrm(path.join(sys_inc, "zlib.h"))
+    os.tryrm(path.join(sys_inc, "zconf.h"))
 
     return true
 end
