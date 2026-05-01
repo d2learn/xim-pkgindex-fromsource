8e083285f533608921b658cd971a0b647343e822
SPeak <speakshen@163.com>
2026-05-02 01:46:08 +0800

    feat(libffi): drop configure-project-installer, inline autotools build (#63)
    Switch fromsource:libffi to its own inline autotools build, applying
    the cluster-A sandbox-fix template:
    
    * paths derived from pkginfo.install_file()
    * configure + make + install chained in single sh -c
    * fixed -j8 (os.cpuinfo nil)
    * sysroot include dir wrapped in _sys_usr_includedir() helper
    * glob-cp via shell instead of os.cp(path/*, dst) (silent no-op in 0.4.9)
    * declare upstream url + sha256 (mirroring scode:libffi's github source)
    * license corrected from URL form to SPDX MIT
    
    Build flag: --disable-exec-static-tramp.
    This works around a libffi 3.4.4 build break on gcc 14+ where
    src/tramp.c references open_temp_exec_file (defined later in
    src/closures.c) without a forward decl, hitting
    -Werror=implicit-function-declaration.
    
    Verified end-to-end in xlings 0.4.9 iso:
      ✓ configure -> make -j8 -> make install
      ✓ produced libffi.so.8.1.2 + libffi.so/libffi.so.8 symlinks
      ✓ ffi.h, ffitarget.h headers shell-copied to subos sysroot
    
    Cluster B leaf 2/8.

--- pkgs/l/libffi.lua
+++ pkgs/l/libffi.lua
@@ -1,3 +1,7 @@
+function __libffi_url(version)
+    return format("https://github.com/libffi/libffi/releases/download/v%s/libffi-%s.tar.gz", version, version)
+end
+
 package = {
     spec = "1",
 
@@ -7,7 +11,7 @@ package = {
     description = "Portable Foreign Function Interface Library",
 
     authors = "Anthony Green, Red Hat, Inc and others.",
-    licenses = "https://github.com/libffi/libffi/blob/master/LICENSE",
+    licenses = "MIT",
     repo = "https://github.com/libffi/libffi",
 
     type = "package",
@@ -23,12 +27,14 @@ package = {
         linux = {
             deps = {
                 "xim:xpkg-helper@0.0.1",
-                "xim:configure-project-installer@0.0.1",
                 "xim:gcc@15.1.0",
                 "xim:make@4.3",
             },
             ["latest"] = { ref = "3.4.4" },
-            ["3.4.4"] = {},
+            ["3.4.4"] = {
+                url = __libffi_url("3.4.4"),
+                sha256 = nil,
+            },
         },
     },
 }
@@ -45,13 +51,33 @@ local libs = {
     "libffi.a",
 }
 
-local sys_usr_includedir = path.join(system.subos_sysrootdir(), "usr/include")
+local function _sys_usr_includedir()
+    return path.join(system.subos_sysrootdir(), "usr/include")
+end
 
 function install()
-    local xpkg = package.name .. "@" .. pkginfo.version()
-    os.tryrm(pkginfo.install_dir())
-    system.exec("configure-project-installer " .. pkginfo.install_dir()
-        .. " --xpkg-scode " .. xpkg)
+    -- Sandbox template (PR #49 bzip2): derive paths from pkginfo.install_file()
+    -- since path.absolute is nil; chain configure + make + install in single
+    -- sh -c (os.cd doesn't propagate to system.exec children).
+    local runtime_dir = path.directory(pkginfo.install_file())
+    local scode_dir = path.join(runtime_dir, "libffi-" .. pkginfo.version())
+    local build_dir = path.join(runtime_dir, "build-libffi")
+    local prefix = pkginfo.install_dir()
+
+    os.tryrm(build_dir)
+    os.mkdir(build_dir)
+
+    log.info("Configuring + building + installing libffi (autotools)...")
+    -- --disable-exec-static-tramp: works around libffi 3.4.4 build break on
+    -- gcc 14+ where src/tramp.c calls open_temp_exec_file (defined later in
+    -- src/closures.c) without a forward decl, hitting -Werror=implicit-function-declaration.
+    system.exec(string.format(
+        "sh -c 'cd %s && %s/configure --prefix=%s --disable-static --enable-shared "
+        .. "--disable-exec-static-tramp "
+        .. "&& make -j8 && make install'",
+        build_dir, scode_dir, prefix
+    ))
+
     return os.isdir(pkginfo.install_dir())
 end
 
@@ -60,10 +86,16 @@ function config()
     xvm.add("libffi-binding-tree")
 
     log.warn("add libs...")
+    -- libffi installs to lib/ on some distros, lib64/ on others; pick whichever is present
+    local libdir = path.join(pkginfo.install_dir(), "lib64")
+    if not os.isdir(libdir) then
+        libdir = path.join(pkginfo.install_dir(), "lib")
+    end
+
     local config = {
         type = "lib",
         version = "libffi-" .. pkginfo.version(),
-        bindir = path.join(pkginfo.install_dir(), "lib64"),
+        bindir = libdir,
         binding = version_tag,
     }
 
@@ -74,11 +106,14 @@ function config()
     end
 
     log.warn("add header files to sysroot...")
+    local sys_inc = _sys_usr_includedir()
+    if not os.isdir(sys_inc) then os.mkdir(sys_inc) end
 
-    local ffi_hdr_dir = path.join(pkginfo.install_dir(), "include")
-    os.mkdir(sys_usr_includedir)
-
-    os.cp(path.join(ffi_hdr_dir, "*"), sys_usr_includedir, { force = true })
+    -- shell cp glob: os.cp(path/*, dst) silently no-ops in 0.4.9 sandbox
+    system.exec(string.format(
+        "sh -c 'cp -f %s/include/*.h %s/'",
+        pkginfo.install_dir(), sys_inc
+    ))
 
     xvm.add("libffi", { binding = version_tag })
 
@@ -92,8 +127,9 @@ function uninstall()
         xvm.remove(lib, "libffi-" .. pkginfo.version())
     end
 
-    os.tryrm(path.join(sys_usr_includedir, "ffi.h"))
-    os.tryrm(path.join(sys_usr_includedir, "ffitarget.h"))
+    local sys_inc = _sys_usr_includedir()
+    os.tryrm(path.join(sys_inc, "ffi.h"))
+    os.tryrm(path.join(sys_inc, "ffitarget.h"))
 
     xvm.remove("libffi-binding-tree")
 
